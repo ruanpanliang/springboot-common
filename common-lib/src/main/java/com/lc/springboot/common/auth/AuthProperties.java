eca3925568acf4cb2f5061355bc7379e52ab3b51
liangchao <ruanpanliang@126.com>
2020-09-18 15:29:44 +0800

    init

--- common-lib/src/main/java/com/lc/springboot/common/auth/AuthProperties.java
+++ common-lib/src/main/java/com/lc/springboot/common/auth/AuthProperties.java
@@ -2,70 +2,55 @@ package com.lc.springboot.common.auth;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/** @author liangchao */
+import java.util.List;
+
+/**
+ * @author liangchao
+ */
 @ApiModel("公共配置类")
 @ConfigurationProperties(prefix = "common.auth.props")
+@Getter
+@Setter
 public class AuthProperties {
 
-  /** AUTH ERROR Messages */
-  @ApiModelProperty(value = "您无此功能操作权限", example = "You do not have access to this service")
-  private String errorMsgDoNotHaveAccess = "您无此功能操作权限";
+    /**
+     * AUTH ERROR Messages
+     */
+    @ApiModelProperty(value = "您无此功能操作权限", example = "You do not have access to this service")
+    private String errorMsgDoNotHaveAccess = "您无此功能操作权限";
 
-  /** Missing Authorization http header */
-  @ApiModelProperty(value = "缺少Authorization信息", example = "Missing Authorization http header")
-  private String errorMsgMissingAuthHeader = "Missing Authorization http header";
+    /**
+     * Missing Authorization http header
+     */
+    @ApiModelProperty(value = "缺少Authorization信息", example = "Missing Authorization http header")
+    private String errorMsgMissingAuthHeader = "Missing Authorization http header";
 
-  /** Invalid Authorization Header */
-  @ApiModelProperty(value = "非法的Authorization信息", example = "Invalid Authorization Header")
-  private String errorAuthorizationHeader = "Invalid Authorization Header";
+    /**
+     * Invalid Authorization Header
+     */
+    @ApiModelProperty(value = "非法的Authorization信息", example = "Invalid Authorization Header")
+    private String errorAuthorizationHeader = "Invalid Authorization Header";
 
-  /** 令牌过期时间，单位：秒，默认24小时 */
-  @ApiModelProperty(value = "令牌过期时间，单位：秒", example = "86400")
-  private long accessTokenValiditySeconds = 86400;
+    /**
+     * 令牌过期时间，单位：秒，默认24小时
+     */
+    @ApiModelProperty(value = "令牌过期时间，单位：秒", example = "86400")
+    private long accessTokenValiditySeconds = 86400;
 
-  /** Session timed out, please log in again */
-  @ApiModelProperty(value = "会话超时，请重新登录")
-  private String accessTokenTimeout = "会话超时，请重新登录";
+    /**
+     * Session timed out, please log in again
+     */
+    @ApiModelProperty(value = "会话超时，请重新登录")
+    private String accessTokenTimeout = "会话超时，请重新登录";
 
-  public String getErrorMsgDoNotHaveAccess() {
-    return errorMsgDoNotHaveAccess;
-  }
+    /**
+     * 白名单IP列表，对于这些IP不做权限校验（例如token校验或用户权限校验等）
+     */
+    @ApiModelProperty(value = "白名单IP列表")
+    private List<String> whiteIpList;
 
-  public void setErrorMsgDoNotHaveAccess(String errorMsgDoNotHaveAccess) {
-    this.errorMsgDoNotHaveAccess = errorMsgDoNotHaveAccess;
-  }
-
-  public String getErrorMsgMissingAuthHeader() {
-    return errorMsgMissingAuthHeader;
-  }
-
-  public void setErrorMsgMissingAuthHeader(String errorMsgMissingAuthHeader) {
-    this.errorMsgMissingAuthHeader = errorMsgMissingAuthHeader;
-  }
-
-  public String getErrorAuthorizationHeader() {
-    return errorAuthorizationHeader;
-  }
-
-  public void setErrorAuthorizationHeader(String errorAuthorizationHeader) {
-    this.errorAuthorizationHeader = errorAuthorizationHeader;
-  }
-
-  public long getAccessTokenValiditySeconds() {
-    return accessTokenValiditySeconds;
-  }
-
-  public void setAccessTokenValiditySeconds(long accessTokenValiditySeconds) {
-    this.accessTokenValiditySeconds = accessTokenValiditySeconds;
-  }
-
-  public String getAccessTokenTimeout() {
-    return accessTokenTimeout;
-  }
-
-  public void setAccessTokenTimeout(String accessTokenTimeout) {
-    this.accessTokenTimeout = accessTokenTimeout;
-  }
 }
--- common-lib/src/main/java/com/lc/springboot/common/auth/AuthorizeInterceptor.java
+++ common-lib/src/main/java/com/lc/springboot/common/auth/AuthorizeInterceptor.java
@@ -1,5 +1,7 @@
 package com.lc.springboot.common.auth;
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.lc.springboot.common.utils.SysUtil;
 import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,40 +16,49 @@ import javax.servlet.http.HttpServletResponse;
  */
 public abstract class AuthorizeInterceptor extends HandlerInterceptorAdapter {
 
-  protected AuthProperties authProperties;
+    protected AuthProperties authProperties;
 
-  public AuthorizeInterceptor(AuthProperties authProperties) {
-    this.authProperties = authProperties;
-  }
-
-  @Override
-  public boolean preHandle(
-      HttpServletRequest request, HttpServletResponse response, Object handler) {
-    if (!(handler instanceof HandlerMethod)) {
-      return true;
+    public AuthorizeInterceptor(AuthProperties authProperties) {
+        this.authProperties = authProperties;
     }
 
-    HandlerMethod handlerMethod = (HandlerMethod) handler;
-    Authorize authorize = handlerMethod.getMethod().getAnnotation(Authorize.class);
-    if (authorize == null) {
-      // 不需要做权限校验
-      return true;
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        // 如果在白名单里，直接放行
+        if (CollectionUtil.isNotEmpty(authProperties.getWhiteIpList())) {
+            String ip = SysUtil.getRemoteAddrIp();
+            if (authProperties.getWhiteIpList().contains(ip)) {
+                return true;
+            }
+        }
+
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Authorize authorize = handlerMethod.getMethod().getAnnotation(Authorize.class);
+        if (authorize == null) {
+            // 不需要做权限校验
+            return true;
+        }
+
+        String authz = AuthContext.getAuthz();
+
+        if (StringUtils.isEmpty(authz)) {
+            throw new PermissionDeniedException(authProperties.getErrorMsgMissingAuthHeader());
+        }
+
+        return checkUser(authz, authorize.value());
     }
 
-    String authz = AuthContext.getAuthz();
-
-    if (StringUtils.isEmpty(authz)) {
-      throw new PermissionDeniedException(authProperties.getErrorMsgMissingAuthHeader());
-    }
-
-    return checkUser(authz,authorize.value());
-  }
-
-  /**
-   * 检验用户是否是合法的用户
-   *
-   * @param authzHeader 请求头token信息
-   * @return boolean 如果验证成功返回true，否则返回false
-   */
-  public abstract boolean checkUser(String authzHeader,String[] authValues);
+    /**
+     * 检验用户是否是合法的用户
+     *
+     * @param authzHeader 请求头token信息
+     * @return boolean 如果验证成功返回true，否则返回false
+     */
+    public abstract boolean checkUser(String authzHeader, String[] authValues);
 }
