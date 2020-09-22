64c920a6131dbd98bfa890ee109a52454c4744a9
liangchao <ruanpanliang@126.com>
2020-09-22 15:29:05 +0800

    init

---
+++ common-lib/src/main/java/com/lc/springboot/common/constant/SpringBootCommon.java
@@ -0,0 +1,14 @@
+package com.lc.springboot.common.constant;
+
+/**
+ * @author liangc
+ * @version V1.0
+ * @date 2020/9/22
+ **/
+public class SpringBootCommon {
+
+    /**
+     * 组件扫包范围
+     */
+    public static final String PACKAGE = "com.lc.springboot";
+}
